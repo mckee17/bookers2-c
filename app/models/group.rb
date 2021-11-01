292c467b547422b59fc49ada7217f08fe647f129
mckee17 <hayate0173@gamil.com>
2021-11-01 11:37:16 +0000

    [add]groupshow

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,10 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  
+  def group_by?(user)
+      group_users.where(user_id: user.id).exists?
+  end
 
   attachment :image
   
